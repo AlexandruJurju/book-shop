38d2196f2ad3f84a42597c8e85780207c2424394
AlexandruJurju <alexandru.i.jurju@gmail.com>
2026-04-19 13:32:57 +0300

    feat(building-blocks): Added Slow Query Interceptor

---
+++ src/BuildingBlocks/BuildingBlocks.Infrastructure/Data/EntityFramework/Interceptors/SlowQueryInterceptor.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace BuildingBlocks.Infrastructure.Data.EntityFramework.Interceptors;
+
+public sealed class SlowQueryInterceptor(
+    ILogger<SlowQueryInterceptor> logger
+) : DbCommandInterceptor
+{
+    private const int SlowQueryThresholdMs = 200;
+
+    public override DbDataReader ReaderExecuted(DbCommand command,
+        CommandExecutedEventData eventData,
+        DbDataReader result
+    )
+    {
+        if (eventData.Duration.TotalMilliseconds >= SlowQueryThresholdMs)
+        {
+            logger.LogWarning("Slow Query ({Duration} ms): {Command}", eventData.Duration.TotalMilliseconds, command.CommandText);
+        }
+
+        return base.ReaderExecuted(command, eventData, result);
+    }
+
+    public override ValueTask<DbDataReader> ReaderExecutedAsync(DbCommand command,
+        CommandExecutedEventData eventData,
+        DbDataReader result,
+        CancellationToken cancellationToken = new()
+    )
+    {
+        if (eventData.Duration.TotalMilliseconds >= SlowQueryThresholdMs)
+        {
+            logger.LogWarning("Slow Query ({Duration} ms): {Command}", eventData.Duration.TotalMilliseconds, command.CommandText);
+        }
+
+        return base.ReaderExecutedAsync(command, eventData, result, cancellationToken);
+    }
+}
