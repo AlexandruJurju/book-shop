c44f1bbfad2ccf3ca4869edb1e09875ec17d3f42
AlexandruJurju <alexandru.i.jurju@gmail.com>
2026-06-11 17:56:46 +0300

    chore: update package versions

--- src/Modules/Catalog/BookShop.Catalog/DependencyInjection.cs
+++ src/Modules/Catalog/BookShop.Catalog/DependencyInjection.cs
@@ -20,20 +20,16 @@ public static class DependencyInjection
         return builder.Services;
     }
 
-    private static IServiceCollection AddPresentation(this IServiceCollection services)
+    private static void AddPresentation(this IServiceCollection services)
     {
         services.AddEndpoints(typeof(DependencyInjection).Assembly);
-
-        return services;
     }
 
-    private static IServiceCollection AddInfrastructure(this IHostApplicationBuilder builder)
+    private static void AddInfrastructure(this IHostApplicationBuilder builder)
     {
         IServiceCollection services = new ServiceCollection();
 
         builder.AddCustomPostgresDbContext<CatalogDbContext>(Resources.Postgres, Services.Catalog);
         services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<CatalogDbContext>());
-
-        return services;
     }
 }
